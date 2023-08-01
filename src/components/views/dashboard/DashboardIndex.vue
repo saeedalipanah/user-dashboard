e5d4b0c74bf801eb1019c7f86d42754267559064
saeedalipanah <saeedalipanah1374@gmail.com>
2023-08-01 21:22:24 +0330

    test provide inject

--- src/components/views/dashboard/DashboardIndex.vue
+++ src/components/views/dashboard/DashboardIndex.vue
@@ -3,11 +3,15 @@
 import NewUser from './NewUser.vue'
 import UserInfo from './UserInfo.vue'
 import Clock from './Clock.vue'
-
+import { inject } from 'vue'
 import { useProfileStore } from '@/stores/profile'
 import { computed, onMounted } from 'vue'
 const store = useProfileStore()
-const username = computed<string>(()=> store.username)
+const username = computed<string>(() => store.username)
+
+//provide and inject test for custom event
+const payload :object = { id: 'gg', name: 'payload' }
+const onButtonClickTest = inject('testFunc') as any
 onMounted(() => {
   store.getUserName()
 })
@@ -18,5 +22,8 @@ onMounted(() => {
     <clock></clock>
     <new-user v-if="!username"></new-user>
     <user-info v-else></user-info>
+
+    <!-- provide and inject test for custom event -->
+    <button @click="onButtonClickTest(payload)">test the emit</button>
   </div>
 </template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -4,8 +4,17 @@ import DashboardIndex from '@/components/views/dashboard/DashboardIndex.vue'
 // components
 import { useRouter } from 'vue-router'
 import { i18n } from '@/composables/I18nCompose'
+import {provide} from 'vue'
 const router = useRouter()
 const { t } = i18n()
+
+
+//provide and inject test for custom event
+const onButtonClickTest  = (payload : object) =>{
+  console.log('its clicked');
+  console.log('payload',payload);
+}
+provide('testFunc', onButtonClickTest)
 </script>
 
 <template>
@@ -15,6 +24,6 @@ const { t } = i18n()
       :sub-title="t('dashboard.subtitle')"
       @back="router.go(-1)"
     />
-    <dashboard-index></dashboard-index>
+    <dashboard-index ></dashboard-index>
   </div>
 </template>
